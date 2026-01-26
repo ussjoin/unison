1fe1eb18bceba976b66df9cfaca0fcfe4e34e207
Brendan O'Connor <brendan@maliceafterthought.com>
2026-01-26 08:40:16 -0800

    Fixing minor bug

--- update_all.sh
+++ update_all.sh
@@ -3,7 +3,7 @@ set -eux pipefail
 
 for f in calibre-web gitlab pocorgtfo powerdata spis wavelog;
 do
-	cd $f
+	cd /mnt/containers/$f
 	docker compose pull && docker compose down && docker compose up -d
 	cd ..
 done
