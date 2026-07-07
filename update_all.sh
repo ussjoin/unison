7da1b4d89d46b1d1217a241c52d7ee63fcb51bdb
Brendan O'Connor <brendan@maliceafterthought.com>
2026-07-07 13:52:17 -0700

    Adding more things to automatic updates

--- update_all.sh
+++ update_all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eux pipefail
 
-for f in calibre-web matrix pocorgtfo powerdata spis;
+for f in calibre-web forgejo immich matrix pocorgtfo powerdata spis;
 do
 	cd /mnt/containers/$f
 	docker compose pull && docker compose down && docker compose up -d
